98504bfa054a3f769b2ac58cf542e40ac30a0a4f
Etienne de Foras <etienne.deforas@gmail.com>
2022-07-09 15:31:58 +0200

    correct normals orientation

--- SolidLibrary.cpp
+++ SolidLibrary.cpp
@@ -7,28 +7,28 @@ namespace SolidLibrary
 		auto s = new Solid();
 
 		//+z face
-		s->add_triangle(Triangle(-1, 1, 1, 1, 1, 1, 1, -1, 1));
+		s->add_triangle(Triangle( 1, 1, 1, -1, 1, 1, 1, -1, 1));
 		s->add_triangle(Triangle(-1, 1, 1, -1, -1, 1, 1, -1, 1));
 
 		//-z face
 		s->add_triangle(Triangle(-1, 1, -1, 1, 1, -1, 1, -1, -1));
-		s->add_triangle(Triangle(-1, 1, -1, -1, -1, -1, 1, -1, -1));
+		s->add_triangle(Triangle( -1, -1, -1, -1, 1, -1, 1, -1, -1));
 
 		//+x face
-		s->add_triangle(Triangle(1, -1, 1,  1, 1, 1, 1, 1, -1));
+		s->add_triangle(Triangle(  1, 1, 1, 1, -1, 1, 1, 1, -1));
 		s->add_triangle(Triangle(1, -1, 1, 1, -1, -1, 1, 1, -1));
 
 		//-x face
 		s->add_triangle(Triangle(-1, -1, 1, -1,1, 1, -1,1, -1));
-		s->add_triangle(Triangle(-1, -1, 1, -1, -1, -1, -1, 1, -1));
+		s->add_triangle(Triangle( -1, -1, -1, -1, -1, 1, -1, 1, -1));
 
 		//+y face
-		s->add_triangle(Triangle(1, 1, -1, 1, 1, 1,-1,  1, 1));
+		s->add_triangle(Triangle( 1, 1, 1, 1, 1, -1, -1,  1, 1));
 		s->add_triangle(Triangle(1, 1, -1, -1, 1, -1, -1, 1, 1));
 
 		//-y face
 		s->add_triangle(Triangle(1, -1, -1, 1, -1, 1, -1, -1, 1));
-		s->add_triangle(Triangle(1, -1, -1, -1, -1, -1, -1, -1, 1));
+		s->add_triangle(Triangle( -1, -1, -1, 1, -1, -1, -1, -1, 1));
 
 		return s;
 	}
@@ -36,16 +36,16 @@ namespace SolidLibrary
 
 	Solid* create_octahedron()
 	{
-		auto s=new Solid();
+		auto s = new Solid();
 
 		s->add_triangle(Triangle(1, 0, 0, 0, 1, 0, 0, 0, 1));
-		s->add_triangle(Triangle(-1, 0, 0, 0, 1, 0, 0, 0, 1));
-		s->add_triangle(Triangle(1, 0, 0, 0, -1, 0, 0, 0, 1));
-		s->add_triangle(Triangle(1, 0, 0, 0, 1, 0, 0, 0, -1));
+		s->add_triangle(Triangle( 0, 1, 0, -1, 0, 0, 0, 0, 1));
+		s->add_triangle(Triangle( 0, -1, 0, 1, 0, 0, 0, 0, 1));
+		s->add_triangle(Triangle( 0, 1, 0, 1, 0, 0, 0, 0, -1));
 		s->add_triangle(Triangle(-1, 0, 0, 0, -1, 0, 0, 0, 1));
 		s->add_triangle(Triangle(-1, 0, 0, 0, 1, 0, 0, 0, -1));
 		s->add_triangle(Triangle(1, 0, 0, 0, -1, 0, 0, 0, -1));
-		s->add_triangle(Triangle(-1, 0, 0, 0, -1, 0, 0, 0, -1));
+		s->add_triangle(Triangle( 0, -1, 0, -1, 0, 0, 0, 0, -1));
 			
 		return s;
 	}
