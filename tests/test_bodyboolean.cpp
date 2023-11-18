63e7692c1a984be5105c06d7b2e19f09e84e3cfa
Etienne de Foras <etienne.deforas@gmail.com>
2023-11-18 10:03:41 +0100

    better test

--- tests/test_bodyboolean.cpp
+++ tests/test_bodyboolean.cpp
@@ -5,6 +5,9 @@
 
 #include "OBJFile.h"
 
+#include <iostream>
+using namespace std;
+
 ///////////////////////////////////////////////////////////////////////////
 int main()
 {
@@ -12,22 +15,34 @@ int main()
 	BodyFactory::SphereUV sphere2(70.);
 
 	sphere2.transform().set_global_translation(10., 25., 40.);
-
 	sphere.set_color(RGB(255, 0, 0));
 	sphere2.set_color(RGB(0, 255, 0));
 
 	BodyUnion bu;
 	bu.set(sphere, sphere2);
-	OBJFile::save("two_spheres_union.obj", bu.mesh());
+	if (!OBJFile::save("two_spheres_union.obj", bu.mesh()))
+	{
+		cout << "Unable to write two_spheres_union.obj, good path and rights?" << endl;
+		return -1;
+	}
 
 	BodyIntersection bi;
 	bi.set(sphere, sphere2);
-	OBJFile::save("two_spheres_intersection.obj", bi.mesh());
+	if (!OBJFile::save("two_spheres_intersection.obj", bi.mesh()))
+	{
+		cout << "Unable to write two_spheres_intersection.obj, good path and rights?" << endl;
+		return -1;
+	}
 
 	BodyDifference bd;
 	bd.set(sphere, sphere2);
-	OBJFile::save("two_spheres_difference.obj", bd.mesh());
+	if (!OBJFile::save("two_spheres_difference.obj", bd.mesh()))
+	{
+		cout << "Unable to write two_spheres_difference..obj, good path and rights?" << endl;
+		return -1;
+	}
 
+	cout << "Test Finished.";
 	return 0;
 }
 ///////////////////////////////////////////////////////////////////////////
