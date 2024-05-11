ac1da97afb1748055af76908d301e42979497149
Etienne de Foras <etienne.deforas@gmail.com>
2024-05-11 16:17:00 +0200

    wip bezier deg3

--- python_tests/test_rational_bezier.py
+++ python_tests/test_rational_bezier.py
@@ -9,37 +9,62 @@ def B12(t):
     return 2*t*(1-t)
 def B22(t):
     return t**2
-
 def B03(t):
     return (1-t)**3
 def B13(t):
     return 3*t*((1-t)**2)
 def B23(t):
     return 3*(t**2)*(1-t)
-def B23(t):
+def B33(t):
     return t**3
 
+#elevate a degreee 2 bezier to a degree 3 bezier
+# from https://pages.mtu.edu/~shene/COURSES/cs3621/NOTES/spline/Bezier/bezier-elev.html
+def degree_elevation(P2,W2):
+    n=2
+    P3=[]
+    P3.append(P2[0])
+    P3.append(P2[0]*1/(n+1)+P2[1]*(1-1/(n+1)))
+    P3.append(P2[1]*2/(n+1)+P2[2]*(1-2/(n+1)))
+    P3.append(P2[n])
+
+    W3=[] #todo
+    W3.append(W2[0])
+    W3.append(W2[0]*1/(n+1)+W2[1]*(1-1/(n+1)))
+    W3.append(W2[1]*2/(n+1)+W2[2]*(1-2/(n+1)))
+    W3.append(W2[n])
+
+    return P3,W3 # todo W
+
 def rational_bezier_deg2(t,P,W):
-    return (B02(t)*P[0]*W[0]+B12(t)*P[1]*W[1]+B22(t)*P[2]*W[2])/(B02(t)*W[0]+B12(t)*W[1]+B22(t)*W[2])
+    numerator=(B02(t)*P[0]*W[0]+B12(t)*P[1]*W[1]+B22(t)*P[2]*W[2])
+    denumerator=(B02(t)*W[0]+B12(t)*W[1]+B22(t)*W[2])
+    return numerator/denumerator
     
 def rational_bezier_deg3(t,P,W):
-    pass
+    numerator=(B03(t)*P[0]*W[0]+B13(t)*P[1]*W[1]+B23(t)*P[2]*W[2]+B33(t)*P[3]*W[3])
+    denumerator=(B03(t)*W[0]+B13(t)*W[1]+B23(t)*W[2]+B33(t)*W[3])
+    return numerator/denumerator
     
 def parametric_circle(t):
     return (1-t**2)/(1+t**2),(2*t)/(1+t**2)
     
-#ref circle
+#reference circle
 t=np.arange(0,math.pi/2,0.01)
-plt.plot(np.cos(t),np.sin(t),label='ref_circle')
+x=np.cos(t)
+y=np.sin(t)
+plt.plot(x,y,label='ref_circle')
+norm=np.sqrt(x**2+y**2)
+print(f"trigonometric circle max_norm={norm.max()} min_norm={norm.min()}")
 
 #circle parametrization test
 t=np.arange(0,1,0.01)
 x,y=parametric_circle(t)
 plt.plot(x,y,label='parametric_circle')
 norm=np.sqrt(x**2+y**2)
-print(f"max_norm={norm.max()} min_norm={norm.min()}")
+print(f"parametric circle max_norm={norm.max()} min_norm={norm.min()}")
 
-#rational bezier fit test
+#rational bezier deg 2 fit test
 px=[1,1,0]
 py=[0,1,1]
 
@@ -50,7 +75,16 @@ x=rational_bezier_deg2(t,px,w)
 y=rational_bezier_deg2(t,py,w)
 plt.plot(x,y,label='rational_bezier_deg2')
 norm=np.sqrt(x**2+y**2)
-print(f"max_norm={norm.max()} min_norm={norm.min()}")
+print(f"deg2 max_norm={norm.max()} min_norm={norm.min()}")
+
+#convert a deg2 bezier to a deg3 bezier
+px3,w3=degree_elevation(px,w)
+py3,w3=degree_elevation(py,w)
+x=rational_bezier_deg3(t,px3,w3)
+y=rational_bezier_deg3(t,py3,w3)
+plt.plot(x,y,label='rational_bezier_deg3')
+norm=np.sqrt(x**2+y**2)
+print(f"deg3 max_norm={norm.max()} min_norm={norm.min()}")
 
 plt.grid()
 plt.legend()
